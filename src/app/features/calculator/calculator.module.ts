5481ce2394ddc56f62b901a43969e3b3c63c993e
Juro Jurinovic <jjurinovic@gmail.com>
2023-10-25 12:37:28 +0200

    changes in calculator module file

--- src/app/features/calculator/calculator.module.ts
+++ src/app/features/calculator/calculator.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './components/calculator/calculator.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ResultComponent } from './components/result/result.component';
+import { OperatorsComponent } from './components/operators/operators.component';
+import { CalculatorButtonsComponent } from './components/calculator-buttons/calculator-buttons.component'
 
 
 
 @NgModule({
   declarations: [
-    CalculatorComponent
+    CalculatorComponent,
+    ResultComponent,
+    OperatorsComponent,
+    CalculatorButtonsComponent
   ],
   exports: [CalculatorComponent],
   imports: [
